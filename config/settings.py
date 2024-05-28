f287632d9a6bb293e212f051efd381d461f31fc7
Vladimir Titov <tvv-vg@yandex.ru>
2024-05-28 11:23:24 +0300

    Перенос чувствительных данных в окружение .env

--- config/settings.py
+++ config/settings.py
@@ -9,21 +9,22 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3e)ajo*qw(v50v7w@r)spwxs!vt)g!^2l(z$yitdjm)&y(38g+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -80,11 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DRF_1',  # Название БД
-        'USER': 'postgres',  # Пользователь для подключения
-        'PASSWORD': '12345',  # Пароль для этого пользователя
-        'HOST': '127.0.0.1',  # Адрес, на котором развернут сервер БД
-        'PORT': '5432',  # Порт, на котором работает сервер БД
+        'NAME': os.getenv('NAME'),  # Название БД
+        'USER': os.getenv('USER'),  # Пользователь для подключения
+        'PASSWORD': os.getenv('PASSWORD'),  # Пароль для этого пользователя
+        'HOST': os.getenv('HOST'),  # Адрес, на котором развернут сервер БД
+        'PORT': os.getenv('PORT'),  # Порт, на котором работает сервер БД
     }
 }
 
